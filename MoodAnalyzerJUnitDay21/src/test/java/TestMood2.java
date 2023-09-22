78ee23eac65559ae7e5b38d34756fc5e34fcef47
shilpa <shilpabhonde000@gmail.com>
2023-09-22 13:40:35 +0530

    UC3 Complete.

---
+++ MoodAnalyzerJUnitDay21/src/test/java/TestMood2.java
@@ -0,0 +1,16 @@
+import junit.framework.Assert;
+import moodanalysis.MoodAnalyser;
+import org.junit.Test;
+
+public class TestMood2 {
+    @Test
+    public void  testCase(){
+        Assert.assertEquals("Sad Mood.", MoodAnalyser.analyserMood("I am in Sad  Mood."));
+        Assert.assertEquals("Happy Mood.", MoodAnalyser.analyserMood("I am in Any Mood"));
+
+        Assert.assertEquals("Happy Mood.", MoodAnalyser.analyserMood("I am in null Mood"));
+
+        Assert.assertEquals("Happy Mood.", MoodAnalyser.analyserMood("I am in Empty Mood"));
+    }
+}
+
